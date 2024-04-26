ec907aae123ebdef505e08b0e6ab5f53c92bfec5
Jake Rieger <contact.jakerieger@gmail.com>
2024-04-26 02:55:43 -0400

    build error fix

--- Lib/RmlUi/RmlUi_Renderer_GL3.h
+++ Lib/RmlUi/RmlUi_Renderer_GL3.h
@@ -33,97 +33,112 @@
 #include <RmlUi/Core/Types.h>
 
 namespace Gfx {
-struct ShadersData;
+    struct ShadersData;
 }
 
 class RenderInterface_GL3 : public Rml::RenderInterface {
 public:
-	RenderInterface_GL3();
-	~RenderInterface_GL3();
+    RenderInterface_GL3();
+    ~RenderInterface_GL3();
 
-	// Returns true if the renderer was successfully constructed.
-	explicit operator bool() const { return static_cast<bool>(shaders); }
+    // Returns true if the renderer was successfully constructed.
+    explicit operator bool() const {
+        return static_cast<bool>(shaders);
+    }
 
-	// The viewport should be updated whenever the window size changes.
-	void SetViewport(int viewport_width, int viewport_height);
+    // The viewport should be updated whenever the window size changes.
+    void SetViewport(int viewport_width, int viewport_height);
 
-	// Sets up OpenGL states for taking rendering commands from RmlUi.
-	void BeginFrame();
-	void EndFrame();
+    // Sets up OpenGL states for taking rendering commands from RmlUi.
+    void BeginFrame();
+    void EndFrame();
 
-	// Optional, can be used to clear the framebuffer.
-	void Clear();
+    // Optional, can be used to clear the framebuffer.
+    void Clear();
 
-	// -- Inherited from Rml::RenderInterface --
+    // -- Inherited from Rml::RenderInterface --
 
-	void RenderGeometry(Rml::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rml::TextureHandle texture,
-		const Rml::Vector2f& translation) override;
+    void RenderGeometry(Rml::Vertex* vertices,
+                        int num_vertices,
+                        int* indices,
+                        int num_indices,
+                        Rml::TextureHandle texture,
+                        const Rml::Vector2f& translation) override;
 
-	Rml::CompiledGeometryHandle CompileGeometry(Rml::Vertex* vertices, int num_vertices, int* indices, int num_indices,
-		Rml::TextureHandle texture) override;
-	void RenderCompiledGeometry(Rml::CompiledGeometryHandle geometry, const Rml::Vector2f& translation) override;
-	void ReleaseCompiledGeometry(Rml::CompiledGeometryHandle geometry) override;
+    Rml::CompiledGeometryHandle CompileGeometry(Rml::Vertex* vertices,
+                                                int num_vertices,
+                                                int* indices,
+                                                int num_indices,
+                                                Rml::TextureHandle texture) override;
+    void RenderCompiledGeometry(Rml::CompiledGeometryHandle geometry,
+                                const Rml::Vector2f& translation) override;
+    void ReleaseCompiledGeometry(Rml::CompiledGeometryHandle geometry) override;
 
-	void EnableScissorRegion(bool enable) override;
-	void SetScissorRegion(int x, int y, int width, int height) override;
+    void EnableScissorRegion(bool enable) override;
+    void SetScissorRegion(int x, int y, int width, int height) override;
 
-	bool LoadTexture(Rml::TextureHandle& texture_handle, Rml::Vector2i& texture_dimensions, const Rml::String& source) override;
-	bool GenerateTexture(Rml::TextureHandle& texture_handle, const Rml::byte* source, const Rml::Vector2i& source_dimensions) override;
-	void ReleaseTexture(Rml::TextureHandle texture_handle) override;
+    bool LoadTexture(Rml::TextureHandle& texture_handle,
+                     Rml::Vector2i& texture_dimensions,
+                     const Rml::String& source) override;
+    bool GenerateTexture(Rml::TextureHandle& texture_handle,
+                         const Rml::byte* source,
+                         const Rml::Vector2i& source_dimensions) override;
+    void ReleaseTexture(Rml::TextureHandle texture_handle) override;
 
-	void SetTransform(const Rml::Matrix4f* transform) override;
+    void SetTransform(const Rml::Matrix4f* transform) override;
 
-	// Can be passed to RenderGeometry() to enable texture rendering without changing the bound texture.
-	static const Rml::TextureHandle TextureEnableWithoutBinding = Rml::TextureHandle(-1);
+    // Can be passed to RenderGeometry() to enable texture rendering without changing the bound
+    // texture.
+    static const Rml::TextureHandle TextureEnableWithoutBinding = Rml::TextureHandle(-1);
 
 private:
-	enum class ProgramId { None, Texture = 1, Color = 2, All = (Texture | Color) };
-	void SubmitTransformUniform(ProgramId program_id, int uniform_location);
+    enum class ProgramId { None, Texture = 1, Color = 2, All = (Texture | Color) };
+    void SubmitTransformUniform(ProgramId program_id, int uniform_location);
 
-	Rml::Matrix4f transform, projection;
-	ProgramId transform_dirty_state = ProgramId::All;
-	bool transform_active = false;
+    Rml::Matrix4f transform, projection;
+    ProgramId transform_dirty_state = ProgramId::All;
+    bool transform_active           = false;
 
-	enum class ScissoringState { Disable, Scissor, Stencil };
-	ScissoringState scissoring_state = ScissoringState::Disable;
+    enum class ScissoringState { Disable, Scissor, Stencil };
+    ScissoringState scissoring_state = ScissoringState::Disable;
 
-	int viewport_width = 0;
-	int viewport_height = 0;
+    int viewport_width  = 0;
+    int viewport_height = 0;
 
-	Rml::UniquePtr<Gfx::ShadersData> shaders;
+    Rml::UniquePtr<Gfx::ShadersData> shaders;
 
-	struct GLStateBackup {
-		bool enable_cull_face;
-		bool enable_blend;
-		bool enable_stencil_test;
-		bool enable_scissor_test;
+    struct GLStateBackup {
+        bool enable_cull_face;
+        bool enable_blend;
+        bool enable_stencil_test;
+        bool enable_scissor_test;
 
-		int viewport[4];
-		int scissor[4];
+        int viewport[4];
+        int scissor[4];
 
-		int stencil_clear_value;
-		float color_clear_value[4];
+        int stencil_clear_value;
+        float color_clear_value[4];
 
-		int blend_equation_rgb;
-		int blend_equation_alpha;
-		int blend_src_rgb;
-		int blend_dst_rgb;
-		int blend_src_alpha;
-		int blend_dst_alpha;
+        int blend_equation_rgb;
+        int blend_equation_alpha;
+        int blend_src_rgb;
+        int blend_dst_rgb;
+        int blend_src_alpha;
+        int blend_dst_alpha;
 
-		struct Stencil {
-			int func;
-			int ref;
-			int value_mask;
-			int writemask;
-			int fail;
-			int pass_depth_fail;
-			int pass_depth_pass;
-		};
-		Stencil stencil_front;
-		Stencil stencil_back;
-	};
-	GLStateBackup glstate_backup = {};
+        struct Stencil {
+            int func;
+            int ref;
+            int value_mask;
+            int writemask;
+            int fail;
+            int pass_depth_fail;
+            int pass_depth_pass;
+        };
+        Stencil stencil_front;
+        Stencil stencil_back;
+    };
+    GLStateBackup glstate_backup = {};
 };
 
 /**
@@ -131,12 +146,13 @@ private:
  */
 namespace RmlGL3 {
 
-// Loads OpenGL functions. Optionally, the out message describes the loaded GL version or an error message on failure.
-bool Initialize(Rml::String* out_message = nullptr);
+    // Loads OpenGL functions. Optionally, the out message describes the loaded GL version or an
+    // error message on failure.
+    bool Initialize(Rml::String* out_message = nullptr);
 
-// Unloads OpenGL functions.
-void Shutdown();
+    // Unloads OpenGL functions.
+    void Shutdown();
 
-} // namespace RmlGL3
+}  // namespace RmlGL3
 
 #endif
