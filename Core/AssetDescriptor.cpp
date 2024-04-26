a759e0a56bcae18d550b4eb9e3cb6854b82330ea
Jake Rieger <contact.jakerieger@gmail.com>
2024-04-26 18:54:07 -0400

    asset descriptor files w data encoding

---
+++ Core/AssetDescriptor.cpp
@@ -0,0 +1,5 @@
+// Author: Jake Rieger
+// Created: 4/26/2024.
+//
+
+#include "AssetDescriptor.h"
---
+++ Core/AssetDescriptor.h
@@ -0,0 +1,10 @@
+// Author: Jake Rieger
+// Created: 4/26/2024.
+//
+
+#pragma once
+
+#include <cpp-base64/base64.h>
+#include <rapidjson/rapidjson.h>
+
+class AssetDescriptor {};
