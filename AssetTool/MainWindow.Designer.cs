ae94cdd16da1143cddc43f692820f5b439798fcf
Jake Rieger <contact.jakerieger@gmail.com>
2024-04-27 18:55:41 -0400

    asset tool in c#

---
+++ AssetTool/MainWindow.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace AssetTool
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openADFToolStripMenuItem = new ToolStripMenuItem();
+            saveADFToolStripMenuItem = new ToolStripMenuItem();
+            packAssetToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            validateADFToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            togglePreviewToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            splitContainer1 = new SplitContainer();
+            groupBox2 = new GroupBox();
+            groupBox1 = new GroupBox();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, viewToolStripMenuItem, toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1054, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openADFToolStripMenuItem, saveADFToolStripMenuItem, packAssetToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openADFToolStripMenuItem
+            // 
+            openADFToolStripMenuItem.Name = "openADFToolStripMenuItem";
+            openADFToolStripMenuItem.Size = new Size(139, 22);
+            openADFToolStripMenuItem.Text = "Open ADF";
+            // 
+            // saveADFToolStripMenuItem
+            // 
+            saveADFToolStripMenuItem.Name = "saveADFToolStripMenuItem";
+            saveADFToolStripMenuItem.Size = new Size(139, 22);
+            saveADFToolStripMenuItem.Text = "Save ADF";
+            // 
+            // packAssetToolStripMenuItem
+            // 
+            packAssetToolStripMenuItem.Name = "packAssetToolStripMenuItem";
+            packAssetToolStripMenuItem.Size = new Size(139, 22);
+            packAssetToolStripMenuItem.Text = "Pack Asset...";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(136, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(139, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { validateADFToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(46, 20);
+            editToolStripMenuItem.Text = "Tools";
+            // 
+            // validateADFToolStripMenuItem
+            // 
+            validateADFToolStripMenuItem.Name = "validateADFToolStripMenuItem";
+            validateADFToolStripMenuItem.Size = new Size(140, 22);
+            validateADFToolStripMenuItem.Text = "Validate ADF";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { togglePreviewToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(44, 20);
+            viewToolStripMenuItem.Text = "View";
+            // 
+            // togglePreviewToolStripMenuItem
+            // 
+            togglePreviewToolStripMenuItem.Name = "togglePreviewToolStripMenuItem";
+            togglePreviewToolStripMenuItem.Size = new Size(153, 22);
+            togglePreviewToolStripMenuItem.Text = "Toggle Preview";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(12, 20);
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 24);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(groupBox2);
+            splitContainer1.Panel1.Controls.Add(groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.BackColor = SystemColors.AppWorkspace;
+            splitContainer1.Size = new Size(1054, 537);
+            splitContainer1.SplitterDistance = 509;
+            splitContainer1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox2.Location = new Point(12, 177);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(485, 348);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Asset Data";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.Location = new Point(12, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(485, 168);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Metadata";
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1054, 561);
+            Controls.Add(splitContainer1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainWindow";
+            Text = "Asset Tool";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openADFToolStripMenuItem;
+        private ToolStripMenuItem saveADFToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem togglePreviewToolStripMenuItem;
+        private ToolStripMenuItem packAssetToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem validateADFToolStripMenuItem;
+        private SplitContainer splitContainer1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+    }
+}
---
+++ AssetTool/MainWindow.cs
@@ -0,0 +1,15 @@
+namespace AssetTool
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
