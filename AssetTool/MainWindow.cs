e4040f8f8b744274dad4b040996f2ebf9e867d69
Jake Rieger <contact.jakerieger@gmail.com>
2024-04-28 13:30:09 -0400

    asset tool fixes

--- AssetTool/MainWindow.cs
+++ AssetTool/MainWindow.cs
@@ -1,20 +1,14 @@
 using K4os.Compression.LZ4;
-using K4os.Compression.LZ4.Encoders;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
-namespace AssetTool
-{
-    public partial class MainWindow : Form
-    {
+namespace AssetTool {
+    public partial class MainWindow : Form {
         public IAssetDescriptor AssetDescriptor { get; set; }
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
 
             ComType.SelectedIndex = 0;
-            AssetDescriptor = new SpriteDescriptor
-            {
+            AssetDescriptor = new SpriteDescriptor {
                 Name = "Nugget",
                 AssetType = AssetType.Sprite,
                 Version = 1
@@ -24,53 +18,50 @@ namespace AssetTool
             ComType.SelectedIndex = (int)AssetDescriptor.AssetType;
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
             Close();
         }
 
-        private void BtnEncodeFile_Click(object sender, EventArgs e)
-        {
+        private void BtnEncodeFile_Click(object sender, EventArgs e) {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "Select source file to encode...";
             var result = ofd.ShowDialog();
-            if (result == DialogResult.OK)
-            {
+            if (result == DialogResult.OK) {
                 var filename = ofd.FileName;
 
-                // do something with selected file
                 byte[] bytes = File.ReadAllBytes(filename);
-                //TxtData.Text = Convert.ToBase64String(bytes);
-                TxtData.Text = BitConverter.ToString(bytes).Replace("-", " ");
+                string bytesAsText;
+                if (bytes.Length > 512) {
+                    bytesAsText = BitConverter.ToString(bytes).Replace("-", " ").Substring(0, 512) + "... (" + (bytes.Length - 512).ToString() + " more)";
+                } else {
+                    bytesAsText = BitConverter.ToString(bytes).Replace("-", " ");
+                }
+                TxtData.Text = bytesAsText;
 
                 AssetDescriptor.Data = bytes;
             }
         }
 
-        private void saveADFToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void saveADFToolStripMenuItem_Click(object sender, EventArgs e) {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Title = "Save ADF";
             saveFileDialog.Filter = "Asset Descriptor File (*.adf)|*.adf";
             var result = saveFileDialog.ShowDialog();
-            if (result == DialogResult.OK)
-            {
+            if (result == DialogResult.OK) {
                 var adf = AssetDescriptor.Serialize();
                 File.WriteAllBytes(saveFileDialog.FileName, [.. adf]);
             }
         }
 
-        private void packAssetToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void packAssetToolStripMenuItem_Click(object sender, EventArgs e) {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Title = "Save Asset Pack";
             saveFileDialog.Filter = "NugPak File (*.nugpak)|*.nugpak";
             var result = saveFileDialog.ShowDialog();
-            if (result == DialogResult.OK)
-            {
+            if (result == DialogResult.OK) {
                 var adf = AssetDescriptor.Serialize();
                 var target = new byte[LZ4Codec.MaximumOutputSize(adf.Count)];
-                var encodeLength = LZ4Codec.Encode([.. adf], target, LZ4Level.L12_MAX);
+                var encodeLength = LZ4Codec.Encode([.. adf], target);
                 File.WriteAllBytes(saveFileDialog.FileName, target);
             }
         }
