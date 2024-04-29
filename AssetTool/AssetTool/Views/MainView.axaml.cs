1f520d521f4978c47c7f6dcc18e2d3b39bfc2abc
Jake Rieger <contact.jakerieger@gmail.com>
2024-04-29 18:14:18 -0400

    asset tool fixes

--- AssetTool/AssetTool/Views/MainView.axaml.cs
+++ AssetTool/AssetTool/Views/MainView.axaml.cs
@@ -1,9 +1,22 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
 
 namespace AssetTool.Views;
 
 public partial class MainView : UserControl {
     public MainView() {
         InitializeComponent();
+    }
+
+    private void BtnExportPak_OnClick(object? sender, RoutedEventArgs e) {
+        return;
+    }
+
+    private void MenuBtnExit_OnClick(object? sender, RoutedEventArgs e) {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime) {
+            lifetime.Shutdown(0);
+        }
     }
 }
