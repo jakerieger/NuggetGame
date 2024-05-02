3c455bac2201d51c9e520f4880b04b1f778b217a
Jake Rieger <contact.jakerieger@gmail.com>
2024-05-02 14:48:34 -0400

    AssetDescriptor -> move func defs

--- XEN-AssetTool/AssetDescriptor.cpp
+++ XEN-AssetTool/AssetDescriptor.cpp
@@ -5,6 +5,160 @@
 #include "AssetDescriptor.h"
 
 namespace AssetTool {
+    IProperties::~IProperties() = default;
+
+    IProperties* SpriteProperties::Deserialize() {
+        return nullptr;
+    }
+
+    std::vector<u8> AudioProperties::Serialize() {
+        std::vector<u8> bytes = {};
+        bytes.resize(GetSize());
+
+        memcpy(bytes.data(), &m_SampleCount, sizeof(u32));
+        memcpy(bytes.data() + sizeof(u32), &m_SampleRate, sizeof(u32));
+        memcpy(bytes.data() + (sizeof(u32) * 2), &m_Channels, sizeof(u32));
+
+        return bytes;
+    }
+
+    IProperties* AudioProperties::Deserialize() {
+        return nullptr;
+    }
+
+    size_t LevelProperties::GetSize() {
+        return sizeof(m_Rows) + sizeof(m_Columns) + (sizeof(f32) * 4);
+    }
+
+    std::vector<u8> LevelProperties::Serialize() {
+        std::vector<u8> bytes = {};
+        bytes.resize(GetSize());
+
+        memcpy(bytes.data(), &m_Rows, sizeof(m_Rows));
+        memcpy(bytes.data() + sizeof(m_Rows), &m_Columns, sizeof(m_Columns));
+
+        const auto playerX = m_PlayerStart.x;
+        const auto playerY = m_PlayerStart.y;
+        const auto objX    = m_ObjectivePosition.x;
+        const auto objY    = m_ObjectivePosition.y;
+
+        memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns), &playerX, sizeof(f32));
+        memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns) + sizeof(f32),
+               &playerY,
+               sizeof(f32));
+
+        memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns) + sizeof(f32) + sizeof(f32),
+               &objX,
+               sizeof(f32));
+        memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns) + sizeof(f32) + sizeof(f32) +
+               sizeof(f32),
+               &objY,
+               sizeof(f32));
+
+        return bytes;
+    }
+
+    IProperties* LevelProperties::Deserialize() {
+        return nullptr;
+    }
+
+    size_t IAssetDescriptor::GetSize() const {
+        return sizeof(m_Type) + m_Name.size() + sizeof(m_Version) + m_SrcData.size() +
+               m_Properties->GetSize() + (sizeof(u32) * 3) -
+               3;  // Why subtract 3? I have no fucking idea, something to do with byte
+        // alignment or some shit
+    }
+
+    IAssetDescriptor::~IAssetDescriptor() {
+        delete m_Properties;
+    }
+
+    SpriteDescriptor::SpriteDescriptor() {
+        this->m_Properties = new SpriteProperties;
+        this->m_Type       = AssetType::Sprite;
+    }
+
+    IAssetDescriptor* SpriteDescriptor::Deserialize(const std::vector<u8>& data) {
+        return nullptr;
+    }
+
+    SpriteProperties* SpriteDescriptor::GetProperties() const {
+        return dynamic_cast<SpriteProperties*>(m_Properties);
+    }
+
+    FontDescriptor::FontDescriptor() {
+        this->m_Properties = new FontProperties;
+        this->m_Type       = AssetType::Font;
+    }
+
+    IAssetDescriptor* FontDescriptor::Deserialize(const std::vector<u8>& data) {
+        return nullptr;
+    }
+
+    FontProperties* FontDescriptor::GetProperties() const {
+        return dynamic_cast<FontProperties*>(m_Properties);
+    }
+
+    AudioDescriptor::AudioDescriptor() {
+        this->m_Properties = new AudioProperties;
+        this->m_Type       = AssetType::Audio;
+    }
+
+    IAssetDescriptor* AudioDescriptor::Deserialize(const std::vector<u8>& data) {
+        return nullptr;
+    }
+
+    AudioProperties* AudioDescriptor::GetProperties() const {
+        return dynamic_cast<AudioProperties*>(m_Properties);
+    }
+
+    LevelDescriptor::LevelDescriptor() {
+        this->m_Properties = new LevelProperties;
+        this->m_Type       = AssetType::Level;
+    }
+
+    IAssetDescriptor* LevelDescriptor::Deserialize(const std::vector<u8>& data) {
+        return nullptr;
+    }
+
+    LevelProperties* LevelDescriptor::GetProperties() const {
+        return dynamic_cast<LevelProperties*>(m_Properties);
+    }
+
+    size_t SpriteProperties::GetSize() {
+        return (sizeof(u32) * 2) + sizeof(bool);
+    }
+
+    std::vector<u8> SpriteProperties::Serialize() {
+        std::vector<u8> bytes = {};
+        bytes.resize(GetSize());
+
+        memcpy(bytes.data(), &m_Width, sizeof(u32));
+        memcpy(bytes.data() + sizeof(u32), &m_Height, sizeof(u32));
+        memcpy(bytes.data() + (sizeof(u32) * 2), &m_IsAlpha, sizeof(bool));
+
+        return bytes;
+    }
+
+    size_t FontProperties::GetSize() {
+        return sizeof(m_DefaultSize);
+    }
+
+    std::vector<u8> FontProperties::Serialize() {
+        std::vector<u8> bytes = {};
+        bytes.resize(GetSize());
+        memcpy(bytes.data(), &m_DefaultSize, sizeof(m_DefaultSize));
+        return bytes;
+    }
+
+    IProperties* FontProperties::Deserialize() {
+        return nullptr;
+    }
+
+    size_t AudioProperties::GetSize() {
+        return sizeof(u32) * 3;
+    }
+
     std::vector<u8> IAssetDescriptor::Serialize() {
         printf("Processing Asset Descriptor '%s'. ", m_Name.c_str());
 
--- XEN-AssetTool/AssetDescriptor.h
+++ XEN-AssetTool/AssetDescriptor.h
@@ -22,7 +22,7 @@ namespace AssetTool {
         virtual size_t GetSize()            = 0;
         virtual std::vector<u8> Serialize() = 0;
         virtual IProperties* Deserialize()  = 0;
-        virtual ~IProperties()              = default;
+        virtual ~IProperties();
     };
 
     class SpriteProperties final : public IProperties {
@@ -31,44 +31,18 @@ namespace AssetTool {
         u32 m_Height   = 0;
         bool m_IsAlpha = false;
 
-        size_t GetSize() override {
-            return (sizeof(u32) * 2) + sizeof(bool);
-        }
-
-        std::vector<u8> Serialize() override {
-            std::vector<u8> bytes = {};
-            bytes.resize(GetSize());
-
-            memcpy(bytes.data(), &m_Width, sizeof(u32));
-            memcpy(bytes.data() + sizeof(u32), &m_Height, sizeof(u32));
-            memcpy(bytes.data() + (sizeof(u32) * 2), &m_IsAlpha, sizeof(bool));
-
-            return bytes;
-        }
-
-        IProperties* Deserialize() override {
-            return nullptr;
-        }
+        size_t GetSize() override;
+        std::vector<u8> Serialize() override;
+        IProperties* Deserialize() override;
     };
 
     class FontProperties final : public IProperties {
     public:
         u32 m_DefaultSize = 0;
 
-        size_t GetSize() override {
-            return sizeof(m_DefaultSize);
-        }
-
-        std::vector<u8> Serialize() override {
-            std::vector<u8> bytes = {};
-            bytes.resize(GetSize());
-            memcpy(bytes.data(), &m_DefaultSize, sizeof(m_DefaultSize));
-            return bytes;
-        }
-
-        IProperties* Deserialize() override {
-            return nullptr;
-        }
+        size_t GetSize() override;
+        std::vector<u8> Serialize() override;
+        IProperties* Deserialize() override;
     };
 
     class AudioProperties final : public IProperties {
@@ -77,24 +51,9 @@ namespace AssetTool {
         u32 m_SampleRate  = 0;
         u32 m_Channels    = 0;
 
-        size_t GetSize() override {
-            return sizeof(u32) * 3;
-        }
-
-        std::vector<u8> Serialize() override {
-            std::vector<u8> bytes = {};
-            bytes.resize(GetSize());
-
-            memcpy(bytes.data(), &m_SampleCount, sizeof(u32));
-            memcpy(bytes.data() + sizeof(u32), &m_SampleRate, sizeof(u32));
-            memcpy(bytes.data() + (sizeof(u32) * 2), &m_Channels, sizeof(u32));
-
-            return bytes;
-        }
-
-        IProperties* Deserialize() override {
-            return nullptr;
-        }
+        size_t GetSize() override;
+        std::vector<u8> Serialize() override;
+        IProperties* Deserialize() override;
     };
 
     class LevelProperties final : public IProperties {
@@ -104,41 +63,9 @@ namespace AssetTool {
         glm::vec<2, f32> m_PlayerStart       = {};
         glm::vec<2, f32> m_ObjectivePosition = {};
 
-        size_t GetSize() override {
-            return sizeof(m_Rows) + sizeof(m_Columns) + (sizeof(f32) * 4);
-        }
-
-        std::vector<u8> Serialize() override {
-            std::vector<u8> bytes = {};
-            bytes.resize(GetSize());
-
-            memcpy(bytes.data(), &m_Rows, sizeof(m_Rows));
-            memcpy(bytes.data() + sizeof(m_Rows), &m_Columns, sizeof(m_Columns));
-
-            const auto playerX = m_PlayerStart.x;
-            const auto playerY = m_PlayerStart.y;
-            const auto objX    = m_ObjectivePosition.x;
-            const auto objY    = m_ObjectivePosition.y;
-
-            memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns), &playerX, sizeof(f32));
-            memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns) + sizeof(f32),
-                   &playerY,
-                   sizeof(f32));
-
-            memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns) + sizeof(f32) + sizeof(f32),
-                   &objX,
-                   sizeof(f32));
-            memcpy(bytes.data() + sizeof(m_Rows) + sizeof(m_Columns) + sizeof(f32) + sizeof(f32) +
-                     sizeof(f32),
-                   &objY,
-                   sizeof(f32));
-
-            return bytes;
-        }
-
-        IProperties* Deserialize() override {
-            return nullptr;
-        }
+        size_t GetSize() override;
+        std::vector<u8> Serialize() override;
+        IProperties* Deserialize() override;
     };
 
     class IAssetDescriptor {
@@ -151,80 +78,35 @@ namespace AssetTool {
 
         std::vector<u8> Serialize();
         virtual IAssetDescriptor* Deserialize(const std::vector<u8>& data) = 0;
-
-        [[nodiscard]] size_t GetSize() const {
-            return sizeof(m_Type) + m_Name.size() + sizeof(m_Version) + m_SrcData.size() +
-                   m_Properties->GetSize() + (sizeof(u32) * 3) -
-                   3;  // Why subtract 3? I have no fucking idea, something to do with byte
-                       // alignment or some shit
-        }
-
-        virtual ~IAssetDescriptor() {
-            delete m_Properties;
-        }
+        [[nodiscard]] size_t GetSize() const;
+        virtual ~IAssetDescriptor();
     };
 
     class SpriteDescriptor final : public IAssetDescriptor {
     public:
-        SpriteDescriptor() {
-            this->m_Properties = new SpriteProperties;
-            this->m_Type       = AssetType::Sprite;
-        }
-
-        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override {
-            return nullptr;
-        }
-
-        [[nodiscard]] SpriteProperties* GetProperties() const {
-            return dynamic_cast<SpriteProperties*>(m_Properties);
-        }
+        SpriteDescriptor();
+        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override;
+        [[nodiscard]] SpriteProperties* GetProperties() const;
     };
 
     class FontDescriptor final : public IAssetDescriptor {
     public:
-        FontDescriptor() {
-            this->m_Properties = new FontProperties;
-            this->m_Type       = AssetType::Font;
-        }
-
-        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override {
-            return nullptr;
-        }
-
-        [[nodiscard]] FontProperties* GetProperties() const {
-            return dynamic_cast<FontProperties*>(m_Properties);
-        }
+        FontDescriptor();
+        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override;
+        [[nodiscard]] FontProperties* GetProperties() const;
     };
 
     class AudioDescriptor final : public IAssetDescriptor {
     public:
-        AudioDescriptor() {
-            this->m_Properties = new AudioProperties;
-            this->m_Type       = AssetType::Audio;
-        }
-
-        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override {
-            return nullptr;
-        }
-
-        [[nodiscard]] AudioProperties* GetProperties() const {
-            return dynamic_cast<AudioProperties*>(m_Properties);
-        }
+        AudioDescriptor();
+        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override;
+        [[nodiscard]] AudioProperties* GetProperties() const;
     };
 
     class LevelDescriptor final : public IAssetDescriptor {
     public:
-        LevelDescriptor() {
-            this->m_Properties = new LevelProperties;
-            this->m_Type       = AssetType::Level;
-        }
-
-        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override {
-            return nullptr;
-        }
-
-        [[nodiscard]] LevelProperties* GetProperties() const {
-            return dynamic_cast<LevelProperties*>(m_Properties);
-        }
+        LevelDescriptor();
+        IAssetDescriptor* Deserialize(const std::vector<u8>& data) override;
+        [[nodiscard]] LevelProperties* GetProperties() const;
     };
 }  // namespace AssetTool
