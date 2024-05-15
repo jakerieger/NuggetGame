655cfc39a9eca6862a041596d8ce251cb46d04a3
Jake Rieger <contact.jakerieger@gmail.com>
2024-05-15 14:38:36 -0400

    more project restructuring

--- Tests/XEN-AssetTool/AssetPak.test.cpp
+++ Tests/XEN-AssetTool/AssetPak.test.cpp
@@ -1,3 +1,10 @@
 // Author: Jake Rieger
 // Created: 5/15/24.
 //
+
+#include <AssetPak.h>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Packer", "[XEN-AssetTool]") {
+    REQUIRE(true);
+}
